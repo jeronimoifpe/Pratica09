6921a7800f64f306e806941b729699956d751fb2
jeronimo <jcm1@discente.ifpe.edu.br>
2023-06-16 21:01:51 -0300

    Passos 01, 02 e 03 da parte 03 da prática 03.

---
+++ app/src/main/java/pdm/pratica09/CityAdapter.java
@@ -0,0 +1,26 @@
+package pdm.pratica09;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CityAdapter extends ArrayAdapter {
+    private City [] cities;
+    public CityAdapter(Context context, int resource, City [] cities) {
+        super(context, resource, cities);
+        this.cities = cities;
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View listItem = inflater.inflate(R.layout.city_listitem, null, true);
+        TextView cityName = listItem.findViewById(R.id.city_name);
+        TextView cityInfo = listItem.findViewById(R.id.city_info);
+        cityName.setText(cities[position].getName());
+        cityInfo.setText(cities[position].getInfo());
+        return listItem;
+    }
+}
