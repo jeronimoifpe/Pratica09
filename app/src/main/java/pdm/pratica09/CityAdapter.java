3daab71af78569bd271f9b97a72285ddab9feb08
jeronimo <jcm1@discente.ifpe.edu.br>
2023-06-16 21:07:50 -0300

    Passos 01, 02 e 03 da parte 05 da prática 03.

--- app/src/main/java/pdm/pratica09/CityAdapter.java
+++ app/src/main/java/pdm/pratica09/CityAdapter.java
@@ -16,16 +16,25 @@ public class CityAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         View listItem = null;
+        ViewHolder holder = null;
         if (view == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             listItem = inflater.inflate(R.layout.city_listitem, null, true);
+            holder = new ViewHolder();
+            holder.cityName = listItem.findViewById(R.id.city_name);
+            holder.cityInfo = listItem.findViewById(R.id.city_info);
+            listItem.setTag(holder);
         } else {
             listItem = view;
+            holder = (ViewHolder)view.getTag();
         }
-        TextView cityName = listItem.findViewById(R.id.city_name);
-        TextView cityInfo = listItem.findViewById(R.id.city_info);
-        cityName.setText(cities[position].getName());
-        cityInfo.setText(cities[position].getInfo());
+        holder.cityName.setText(cities[position].getName());
+        holder.cityInfo.setText(cities[position].getInfo());
         return listItem;
+    }
+
+    static class ViewHolder {
+        TextView cityName;
+        TextView cityInfo;
     }
 }
